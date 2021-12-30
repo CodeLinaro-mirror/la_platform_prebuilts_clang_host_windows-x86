e808b7ac829651e6477ee9c92cbbfee11e90c7ce
Joe Baker-Malone <jbakermalone@google.com>
2021-12-30 15:45:59 -0800

    Delete unused versions of clang
    Test: existing tests should pass
    Bug: 213648391
    Change-Id: I5109158dc4eb401b19e87d9a46b72b395b89a864

--- clang-r433403/lib64/cmake/polly/PollyExports-all.cmake
+++
@@ -1,4 +0,0 @@
-set_target_properties(PollyISL PROPERTIES
-        IMPORTED_LOCATION_RELEASE ${CMAKE_CURRENT_LIST_DIR}/../../../lib/libPollyISL.a)
-set_target_properties(Polly PROPERTIES
-        IMPORTED_LOCATION_RELEASE ${CMAKE_CURRENT_LIST_DIR}/../../../lib/libPolly.a)
